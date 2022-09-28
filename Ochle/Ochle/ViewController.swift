6775040dce2b26fc30002e5acba62a5defeffdbc
Ldoy <leedoy7989@gmail.com>
2022-09-28 18:48:09 +0900

    docs: Add Xcode Projects folder

---
+++ Ochle/Ochle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ochle
+//
+//  Created by Do Yi Lee on 2022/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
