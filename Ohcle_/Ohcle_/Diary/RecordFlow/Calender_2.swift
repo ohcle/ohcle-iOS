3c28f0e8bf3287cd2e73c5303f60dd552d15fab1
Ldoy <leedoy7989@gmail.com>
2022-10-15 15:41:08 +0900

    feat: Add border into locationSearchBar

---
+++ Ohcle_/Ohcle_/Diary/RecordFlow/Calender_2.swift
@@ -0,0 +1,20 @@
+//
+//  Calender_2.swift
+//  Ohcle_
+//
+//  Created by Do Yi Lee on 2022/10/15.
+//
+
+import SwiftUI
+
+struct Calender_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Calender_2_Previews: PreviewProvider {
+    static var previews: some View {
+        Calender_2()
+    }
+}
