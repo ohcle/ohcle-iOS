8d847e897794e91bad0292b57c6657eac972d476
Ldoy <leedoy7989@gmail.com>
2023-04-08 20:19:59 +0900

    feat: Add missing URLs file

--- Ohcle_/Ohcle_/Login/Networking.swift
+++ Ohcle_/Ohcle_/Login/Networking.swift
@@ -14,3 +14,8 @@ func getAccessTokenURL(_ url: URLs) -> URL {
     
     return url
 }
+
+enum URLs: String {
+    case kakaoLogin = "http://ec2-3-37-182-202.ap-northeast-2.compute.amazonaws.com/v1/account/kakao/signin"
+    case appleLogin = "http://ec2-3-37-182-202.ap-northeast-2.compute.amazonaws.com/v1/account/apple/signin"
+}
