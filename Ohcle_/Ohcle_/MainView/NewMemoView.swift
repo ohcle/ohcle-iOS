d4921a66ed110373e1debd4ddaefcdd1b791e69e
ybib <yb359kr@gmail.com>
2023-06-04 03:06:07 +0900

    feat: Add Delete Closure in NewMemoView

--- Ohcle_/Ohcle_/MainView/NewMemoView.swift
+++ Ohcle_/Ohcle_/MainView/NewMemoView.swift
@@ -50,6 +50,8 @@ struct NewMemoView: View {
     @State private var selectedColor: Color = .clear
     @State private var selectedDate: Date = Date()
     
+    var deleteCompletion: ((Int) -> (Void))?
+    
     private let colors: [Color] = [.red, .orange, .yellow,
                                    .green, .blue, .purple, .black, .gray, .white]
     
@@ -88,6 +90,7 @@ struct NewMemoView: View {
                     Task {
                         await deleteMemo(id: self.id)
                         self.isModalView.toggle()
+                        deleteCompletion?(self.id)
                     }
                     
                 } label: {
