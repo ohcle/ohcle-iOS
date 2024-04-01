08e6f72cd349f09c72a6467134d94af01880b970
Ldoy <leedoy7989@gmail.com>
2024-04-01 21:36:39 +0900

    chore

--- Ohcle_/Ohcle_/Data/DataMapping/ClimbingRecordListDTOt+Mapping.swift
+++ Ohcle_/Ohcle_/Data/DataMapping/ClimbingRecordListDTOt+Mapping.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // MARK: - Climbing Data Transfer Object (기존에 CalenderModel이였던 것)
-struct ClimbingMonthRecord: Decodable {
+struct ClimbingMonthRecord {
     let list: [ClimbingRecord]
 }
 
